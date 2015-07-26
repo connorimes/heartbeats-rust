f755928472810addcaf0d21cb3a03d56a15b23af
Connor Imes <connor.k.imes@gmail.com>
2015-07-26 13:35:32 -0500

    Put FFI functions and types in lib.rs

--- src/heartbeat.rs
+++ src/heartbeat.rs
@@ -1,32 +1,7 @@
-use libc::{uint64_t, int64_t, c_void, c_char, c_double, c_longlong};
+use libc::{c_void, c_char};
 use std::ffi::CString;
 use std::ptr;
-
-pub type HeartbeatReadEnergyFn = extern fn(*mut c_void) -> c_longlong;
-
-#[link(name = "hbt-acc-pow")]
-extern {
-    fn heartbeat_acc_pow_init(parent: *mut c_void,
-                              window_size: uint64_t,
-                              buffer_depth: uint64_t,
-                              log_name: *const c_char,
-                              read_energy_fn: Option<HeartbeatReadEnergyFn>,
-                              ref_arg: *mut c_void) -> *mut c_void;
-
-    fn heartbeat_acc(hb: *mut c_void,
-                     user_tag: uint64_t,
-                     work: uint64_t,
-                     accuracy: c_double,
-                     hb_prev: *mut c_void) -> int64_t;
-
-    fn heartbeat_finish(hb: *mut c_void);
-
-    fn hb_get_user_tag(hb: *const c_void) -> uint64_t;
-
-    fn hb_get_window_rate(hb: *const c_void) -> c_double;
-
-    fn hb_get_window_power(hb: *const c_void) -> c_double;
-}
+use super::*;
 
 /// A `Heartbeat` is used for tracking performance/accuracy/power of recurring jobs.
 pub struct Heartbeat {
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,31 @@
 extern crate libc;
 
 pub mod heartbeat;
+
+use libc::{uint64_t, int64_t, c_void, c_char, c_double, c_longlong};
+
+pub type HeartbeatReadEnergyFn = extern fn(*mut c_void) -> c_longlong;
+
+#[link(name = "hbt-acc-pow")]
+extern {
+    pub fn heartbeat_acc_pow_init(parent: *mut c_void,
+	                              window_size: uint64_t,
+	                              buffer_depth: uint64_t,
+	                              log_name: *const c_char,
+	                              read_energy_fn: Option<HeartbeatReadEnergyFn>,
+	                              ref_arg: *mut c_void) -> *mut c_void;
+
+    pub fn heartbeat_acc(hb: *mut c_void,
+	                     user_tag: uint64_t,
+	                     work: uint64_t,
+	                     accuracy: c_double,
+	                     hb_prev: *mut c_void) -> int64_t;
+
+    pub fn heartbeat_finish(hb: *mut c_void);
+
+    pub fn hb_get_user_tag(hb: *const c_void) -> uint64_t;
+
+    pub fn hb_get_window_rate(hb: *const c_void) -> c_double;
+
+    pub fn hb_get_window_power(hb: *const c_void) -> c_double;
+}
